e27c295e39144a799c44ef37fbd5c3b69e860901
Alex Birlogeanu <ibirlogeanu@amberstudio.com>
2018-09-10 18:17:22 +0300

    - Updated to vulkan 1.1.83 - Fixed CreateRenderPass errors for subpass dependecies missmatch

---
+++ VULKAN/DescriptorsUtils.cpp
@@ -0,0 +1,108 @@
+#include "DescriptorsUtils.h"
+
+#include "defines.h"
+#include "Utils.h"
+
+void NewDescriptorPool(const std::vector<VkDescriptorPoolSize>& poolSize, uint32_t maxSets, VkDescriptorPool* descPool)
+{
+	VkDescriptorPoolCreateInfo descPoolCi;
+	cleanStructure(descPoolCi);
+	descPoolCi.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+	descPoolCi.pNext = nullptr;
+	descPoolCi.flags = VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT;
+	descPoolCi.maxSets = maxSets;
+	descPoolCi.poolSizeCount = (uint32_t)poolSize.size();
+	descPoolCi.pPoolSizes = poolSize.data();
+
+	VULKAN_ASSERT(vk::CreateDescriptorPool(vk::g_vulkanContext.m_device, &descPoolCi, nullptr, descPool));
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+//DescriptorSetLayout
+///////////////////////////////////////////////////////////////////////////////////////
+
+DescriptorSetLayout::DescriptorSetLayout()
+	:m_descSetLayoutHandle(VK_NULL_HANDLE)
+{
+
+}
+
+DescriptorSetLayout::~DescriptorSetLayout()
+{
+	vk::DestroyDescriptorSetLayout(vk::g_vulkanContext.m_device, m_descSetLayoutHandle, nullptr);
+}
+
+void DescriptorSetLayout::AddBinding(unsigned int binding, VkDescriptorType type, VkShaderStageFlags flags, unsigned int count)
+{
+	TRAP(m_descSetLayoutHandle == VK_NULL_HANDLE && "Warning!! Layout is already created, this change will have no effect!");
+	m_bindings.push_back(CreateDescriptorBinding(binding, type, flags, count));
+}
+
+void DescriptorSetLayout::Construct()
+{
+	NewDescriptorSetLayout(m_bindings, &m_descSetLayoutHandle);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+//DescriptorSetLayout
+///////////////////////////////////////////////////////////////////////////////////////
+
+DescriptorPool::DescriptorPool()
+	: m_descPoolHandle(VK_NULL_HANDLE)
+	, m_remainingSets(0)
+{
+
+}
+
+DescriptorPool::~DescriptorPool()
+{
+	vk::DestroyDescriptorPool(vk::g_vulkanContext.m_device, m_descPoolHandle, nullptr);
+}
+
+void DescriptorPool::Construct(const DescriptorSetLayout& layoutType, uint32_t maxSets)
+{
+	std::vector<VkDescriptorSetLayoutBinding> bindings = layoutType.GetBindings();
+
+	for (auto binding : bindings)
+		m_descriptorPoolSizeRemaining[binding.descriptorType].descriptorCount += binding.descriptorCount;
+
+	std::vector<VkDescriptorPoolSize> poolSize;
+
+	for (auto& pSize : m_descriptorPoolSizeRemaining)
+		if (pSize.descriptorCount != 0)
+		{
+			pSize.descriptorCount *= maxSets;
+			poolSize.push_back(pSize);
+		}
+
+	m_remainingSets = maxSets;
+
+	NewDescriptorPool(poolSize, m_remainingSets, &m_descPoolHandle);
+}
+
+void DescriptorPool::Construct(const std::vector<VkDescriptorPoolSize>& poolSize, uint32_t maxSets)
+{
+	NewDescriptorPool(poolSize, maxSets, &m_descPoolHandle);
+}
+
+bool DescriptorPool::CanAllocate(const DescriptorSetLayout& layoutType)
+{
+	std::vector<VkDescriptorSetLayoutBinding> bindings = layoutType.GetBindings();
+	std::array<VkDescriptorPoolSize, VkDescriptorType::VK_DESCRIPTOR_TYPE_RANGE_SIZE> poolSizeCopy = m_descriptorPoolSizeRemaining;
+
+	for (auto binding : bindings)
+	{
+		poolSizeCopy[binding.descriptorType].descriptorCount -= binding.descriptorCount;
+		if (poolSizeCopy[binding.descriptorType].descriptorCount < 0)
+			return false;
+	}
+
+	return (m_remainingSets - 1) >= 0;
+}
+
+VkDescriptorSet DescriptorPool::AllocateDescriptorSet(const DescriptorSetLayout& layoutType)
+{
+	VkDescriptorSet newDescSet;
+	AllocDescriptorSets(m_descPoolHandle, layoutType.Get(), &newDescSet);
+	return newDescSet;
+}
---
+++ VULKAN/DescriptorsUtils.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "VulkanLoader.h"
+
+#include <vector>
+#include <array>
+
+class DescriptorSetLayout
+{
+public:
+	DescriptorSetLayout();
+	~DescriptorSetLayout();
+
+	const VkDescriptorSetLayout& Get() const { return m_descSetLayoutHandle; }
+	//VkDescriptorSetLayout& Get() { return m_descSetLayoutHandle; }
+
+	const std::vector<VkDescriptorSetLayoutBinding>& GetBindings() const { return m_bindings; }
+
+	//Construct
+	void AddBinding(unsigned int binding, VkDescriptorType type, VkShaderStageFlags flags, unsigned int count = 1);
+	void Construct();
+private:
+	std::vector<VkDescriptorSetLayoutBinding>	m_bindings;
+	VkDescriptorSetLayout						m_descSetLayoutHandle;
+};
+
+class DescriptorPool
+{
+public:
+	DescriptorPool();
+	virtual ~DescriptorPool();
+
+	const VkDescriptorPool& Get() const { m_descPoolHandle; }
+
+	void Construct(const DescriptorSetLayout& layoutType, uint32_t maxSets);
+	void Construct(const std::vector<VkDescriptorPoolSize>& poolSize, uint32_t maxSets); //backwards compatibility
+
+	bool CanAllocate(const DescriptorSetLayout& layoutType);
+	VkDescriptorSet AllocateDescriptorSet(const DescriptorSetLayout& layoutType);
+private:
+	VkDescriptorPool																	m_descPoolHandle;
+	std::array<VkDescriptorPoolSize, VkDescriptorType::VK_DESCRIPTOR_TYPE_RANGE_SIZE>	m_descriptorPoolSizeRemaining;
+	uint32_t																			m_remainingSets;
+};
